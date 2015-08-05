c9a8fe5b52294982ecdb1d9ef7b5e4b666c874b7
Mehran Hatami <mehranhatamie@gmail.com>
2015-08-06 01:28:46 +0430

    fix host

--- app.py
+++ app.py
@@ -38,4 +38,4 @@ def get_task(task_id):
 port = int(os.environ.get("PORT", 5000))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=port)
+    app.run(debug=True, port=port, host='0.0.0.0')
