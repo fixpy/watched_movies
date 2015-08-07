eb57a49c786bd0be18788b025ad8e04b48451911
Mehran Hatami <mehranhatamie@gmail.com>
2015-08-07 18:06:06 +0430

    back to client folder

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,9 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 env = os.environ.get('ENV', 'development')
 public_folder = '../client'
-print env
-if env == 'production':
-    public_folder = '../public'
+# print env
+# if env == 'production':
+#     public_folder = '../public'
 
 app = Flask(__name__, static_folder=public_folder, static_url_path='')
 app.config.from_object('config')
